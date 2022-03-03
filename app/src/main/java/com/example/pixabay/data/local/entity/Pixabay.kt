4e0b06b74fee23fb7a9b220aac284221983bb31a
Alex-mumo <alexymumo2000@gmail.com>
2022-03-03 10:25:37 +0300

    initial commit

---
+++ app/src/main/java/com/example/pixabay/data/local/entity/Pixabay.kt
@@ -0,0 +1,24 @@
+package com.example.pixabay.data.local.entity
+
+import androidx.room.Entity
+
+@Entity(tableName = "pixabay_table")
+data class Pixabay(
+    val id : Int,
+    val type : String,
+    val tags : String,
+    val webformatURL : String,
+    val views : Int,
+    val downloads : Int,
+    val comments : Int,
+    val likes : Int,
+    val user : String
+)
+
+/*
+Serialization - Process of converting data used by an application to a format that
+can be transferred over network and stored in database file
+Deserialization - reading data from data source and converting it into a runtime object
+
+*
+ */
