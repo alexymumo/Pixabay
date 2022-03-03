449c5845c32647dc3b0053425a2ebdb1d2493cf6
Alex-mumo <alexymumo2000@gmail.com>
2022-03-03 10:38:41 +0300

    added dao and db

---
+++ app/src/main/java/com/example/pixabay/data/local/dao/PixabayDao.kt
@@ -0,0 +1,21 @@
+package com.example.pixabay.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.pixabay.data.local.entity.Pixabay
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PixabayDao {
+
+    @Query("SELECT * FROM pixabay_table")
+    fun fetchAllImages(): Flow<List<Pixabay>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun saveImage(pixabay: List<Pixabay>)
+
+    @Query("DELETE FROM pixabay_table")
+    suspend fun deleteAll()
+}
---
+++ app/src/main/java/com/example/pixabay/data/local/db/PixabayDb.kt
@@ -0,0 +1,15 @@
+package com.example.pixabay.data.local.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.pixabay.data.local.dao.PixabayDao
+import com.example.pixabay.data.local.entity.Pixabay
+
+@Database(
+    entities = [Pixabay::class],
+    exportSchema = false,
+    version = 1
+)
+abstract class PixabayDb: RoomDatabase() {
+    abstract fun pixabay(): PixabayDao
+}
